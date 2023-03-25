6c174a556f0f4cc69e28e2980f031faf528d5433
Anton Rylin <antonkrytoi228@gmail.com>
2023-03-25 23:40:18 +0100

    Update 1
    First update
    
    Initial commit
    
    Deletion

---
+++ ChatUI.py
@@ -0,0 +1,75 @@
+from telethon import TelegramClient, events
+from Replies import replies
+import datetime
+import os
+
+# Log in information
+api_id = 0
+api_hash = ''
+phone_number = ''
+session_name = "ChatBot"
+
+# client Init
+client = TelegramClient(session_name, api_id, api_hash)
+
+# configuration
+auto_save = True  # True to treat all messages as save prompts
+stealth = True  # True to provide no feedback
+
+
+# define an event handler for incoming messages
+@client.on(events.NewMessage(incoming=True))
+async def handle_new_message(event):
+    try:
+
+        # index
+        ind = 1
+
+        # prompt
+        message: str = event.message.text
+        prompt: str = message.split()[0].lower()
+
+        # auto save
+        if auto_save:
+            prompt = "save"
+            ind = 0
+
+        # setup
+        id = event.message.sender_id
+        message_body = " ".join(message.split()[ind:])
+        now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        existing = os.path.join("saved", str(id) + ".txt")
+        timestamp = "\n" + now + ":\n"
+
+        # stop bot option
+        if prompt == "bye":
+            await event.respond((replies[prompt]))
+            client.disconnect()
+
+        # save prompt
+        if prompt == "save":
+
+            try:
+                open(existing, "a").write(timestamp+message_body)
+            except:
+                open(existing, "w").write(timestamp+message_body)
+
+
+
+        # reply / feedback
+        if not stealth:
+            await event.respond((replies[prompt]))
+
+        # id
+        print("Received a message from: "+str(event.message.sender_id)+" - "+" ".join(message_body.split()[0:10])+"...")
+        # delete the received message
+        await client.delete_messages(event.chat_id, event.message)
+
+    except:
+        if not stealth:
+            await event.respond("not understood, try 'help'")
+
+
+# forever running
+client.start()
+client.run_until_disconnected()
---
+++ Replies.py
@@ -0,0 +1,5 @@
+replies = {
+    "save":"saved!",
+    "bye": "Have a nice day!",
+    "help": "save - to save information\nbye - to disconnect\n"
+}
